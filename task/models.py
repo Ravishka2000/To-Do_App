1425809db9701c934aa54bf940761c7d255a2556
Ravishka2000 <gamageravishkadulshan2000@gmail.com>
2022-07-12 18:36:19 +0530

    initial

--- task/models.py
+++ task/models.py
@@ -6,3 +6,7 @@ class Todo(models.Model):
     description = models.CharField(max_length=150)
     date = models.DateTimeField(auto_now=True)
     due_date = models.DateTimeField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.task_title}'
