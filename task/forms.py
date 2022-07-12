6019eae36673b7aa3425d40ed81a427d08ed95b9
Ravishka2000 <gamageravishkadulshan2000@gmail.com>
2022-07-12 21:42:17 +0530

    Display from DB

--- task/forms.py
+++ task/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from .models import Todo
 
 
 class TodoForm(forms.ModelForm):
     class Meta:
-
+        model = Todo
+        fields = ['task_title', 'description', 'due_date', 'status']
+        labels = {
+            'task_title': 'Title',
+            'description': 'Description',
+            'due_date': 'Due Date',
+        }
+        widgets = {
+            'task_title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.TextInput(attrs={'class': 'form-control'}),
+            'due_date': forms.DateTimeInput(attrs={'class': 'form-control'}),
+        }
--- task/views.py
+++ task/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'task/index.html')
+    return render(request, 'task/index.html', {
+        'todos': Todo.objects.all()
+    })
