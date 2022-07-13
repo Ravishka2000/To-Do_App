392cdcea3a0d99bf883c3ccc58ee7e59aa852325
Ravishka2000 <gamageravishkadulshan2000@gmail.com>
2022-07-13 12:55:03 +0530

    Fkin Errors

--- task/views.py
+++ task/views.py
@@ -1,8 +1,34 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Todo
+from .forms import TodoForm
 
 
 def index(request):
     return render(request, 'task/index.html', {
         'todos': Todo.objects.all()
     })
+
+
+def view_tasks(request, id):
+    todo = Todo.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def updatetask(request, id):
+    if request.method == 'POST':
+        todo = Todo.objects.get(pk=id)
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return render(request, 'task/updatetask.html',{
+                'form': form,
+                'success': True,
+            })
+        else:
+            todo = Todo.objects.get(pk=id)
+            form = TodoForm(instance=todo)
+        return render(request, 'task/updatetask.html', {
+            'form': form,
+        })
